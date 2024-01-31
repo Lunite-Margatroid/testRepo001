480faef557ad6bc1a58984c9bbf8d664ba1e2ab7
Lunite_Margatroid <534830021@qq.com>
2024-01-31 23:54:35 +0800

    完成swap和add和minas

--- application.cpp
+++ application.cpp
@@ -7,6 +7,17 @@ void swap(int* a, int *b)
 	*a = *a ^ *b;
 }
 
+void add(int a, int b)
+{
+	return a+b;
+}
+
+void minas(int a, int b)
+{
+	
+	return a - b;
+}
+
 int main()
 {
 	std::cout << "Hello world." << std::endl;
